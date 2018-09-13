cb64e9f887d35ef74414c5d811172f985c883141
walli <wallypark710@gmail.com>
2018-09-14 01:01:25 +0900

    write basic function, compare and switch

--- logic.js
+++ logic.js
@@ -1,13 +1,48 @@
 
+var startWord = ['자몽', '원두', '커피', '피아노', '여행'];
+
+var randomIdx = function(arr){
+	return parseInt(Math.random()*(arr.length));
+}
 
 
+var initGame = function(target){
+	var $space = $('<div></div>').addClass('word').text(target);
+
+	$('.word_space').text('');
+	$space.appendTo($('.word_space'));
+
+	// 시간초기화.
+}
+
+var confirmWord = function(target, input){
+	if( target[target.length-1] === input[0] ){
+		return true;
+	} else {
+		return false;
+	}
+}
 
 
 
 $(document).ready(function(){
+	
+	var target = startWord[randomIdx(startWord)];
+	var cnt = 0;
 
+	initGame(target);
+	
 
+	$('.btn').click(function(){
+		var inputWord = $('.textbox').val();
+		if( confirmWord(target,inputWord) ){
+			$('.word').text(inputWord);
+			target = inputWord;
+		} else {
+			console.log("틀렸어");
+		}
 
+	});
 
 
 
